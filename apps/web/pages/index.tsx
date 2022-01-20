895ccf32b09afbc9471b26e2bba79396574445e4
Khoirul Asfian <45413544+asfian99@users.noreply.github.com>
2022-01-21 01:24:08 +0700

    fix(global): fix tailwindcss jit for shared ui package

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,11 +2,9 @@ import { Button } from "ui";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center w-full">
       <h1 className="text-center my-4 text-2xl font-bold">Web</h1>
-      <div className="mx-auto w-full">
-        <Button>Shared Button UI</Button>
-      </div>
+      <Button>Shared Button UI</Button>
     </div>
   );
 }
--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -5,6 +5,8 @@ interface Props {
 
 export const Button = ({ children }: Props) => {
   return (
-    <button className="px-6 py-3 bg-emerald-600 text-white">{children}</button>
+    <button className="bg-emerald-600 hover:bg-emerald-700 px-6 py-3 text-white rounded-md">
+      {children}
+    </button>
   );
 };
